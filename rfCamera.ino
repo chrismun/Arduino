8abf18b74d80fff138bb66ffd70439e9f363db5b
chrismun <47751224+chrismun@users.noreply.github.com>
2024-10-25 23:28:35 -0700

    Update rfCamera.ino

--- rfCamera.ino
+++ rfCamera.ino
@@ -4,47 +4,7 @@
 #include <SPI.h>
 #include "memorysaver.h"
 
-/*
-  Author: Chris Munley
-  I wrote this code in an attempt to send pictures over Xbee Series 2 RF modules in tandem with arduino. The code should work, as it was put together
-  by taking pieces of code from the Arducam library (found on github) along with a few other resouces online. 
-  I ran into a few errors writing this code. first, sometimes I would get the SPI Interface error, and I am not sure why. If you get this error
-  I suggest either resetting the arducam by unplugging and plugging everything back in and re uploading the sketch to the arduino, also make sure that
-  CS pin is set to 7 on the arduino. 
-  Also, when sending the bitstream over the rf modules sometimes every bit would become 00 for me, but I think that was due to something like
-  the interface error because I struggled to get past all of the confirmation steps that the camera is working in this code.
-  The bitstream has to be reconstructed on the PC end after it is compressed and sent, which can probably be done with MATLAB but I am not 
-  familiar with it and did not get to try that. 
-  https://www.imperialviolet.org/binary/jpeg/   this page and the jpeg wiki should help with rebuilding the bitstream
-  you can test the camera to see if it can take pictures at all with the software in the arducam library
-  found at: arducam>examples>host_app>ArduCAM_Host_V2.0_Windows
-
-  I also had some troubles with the header files in this code, sometimes they were not found and I am not sure why. Possibly had to do with how I 
-  downloaded the library but that I never figured out. I also could not get any of the example codes to work with this setup, which is weird
-  because they should have worked. My assumption is that if you can get the example codes in the arducam library to work, you should be able to 
-  send the bitsream across the xbees with no troubles, as that is how the code is set up
-
-  In my attempts to make this code work, I removed a few features that were included in the example code such as options to change the resolution of the
-  picture. Also, I think this code now only works with the OV5642, so if you use a different camera you will have to go back to the example codes and
-  figure out what you need to use for a different camera. I tried to remove as much extra code as I could without damaging the integrity of the program,
-  so if you go back to the example code if you are really struggling you can see all of the steps you are supposed to take to get the camera to work because
-  it is possible I left something important out. Also, some parts of the code might be extraneous as I did not want to mess up how it worked, so the code could
-  definitely use some reworking if you are really dedicated.
-
-  You have to configure the xbees before using them, which I did so they should be good to go, but make sure that all of the baud rates match up so that
-  they can understand eachother. 
-
-  To take this code further I would add functionality to save pictures to the SD card since it does not have that yet, and also I would look into power
-  saving options since it is supposed to run off solar. Also, I would work on a way to quickly change the rate of pictures, and some type of input to tell
-  the arduino if you want to recieve the pictures now or save them to sd card and send them over once you take a certain amount.
-
-  Another advantageous function of the camera would be to add functionality to wire directly to the camera so that you can get 
-  live video without waiting on the xbee to catch up. This would be beneficial so that when setting up the camera you can tell exactly 
-  what it is looking at. 
-  
-  If you want to talk to me about the code email me at chrismunley1@gmail.com I will be more than happy to help, I would love to see this project working
-  after I struggled for hours on hours troubleshooting errors. 
-*/
+/* send pictures over Xbee Series 2 RF modules with arduino (done in 2017) */
 
 //This demo can only work on OV2640_MINI_2MP or OV5642_MINI_5MP or OV5642_MINI_5MP_BIT_ROTATION_FIXED platform.
 // #if !(defined OV5642_MINI_5MP || defined OV5642_MINI_5MP_BIT_ROTATION_FIXED || defined OV2640_MINI_2MP) 
@@ -73,7 +33,7 @@ void setup() {
 #else
   Wire.begin();
   Serial.begin(115200);
-  Xbee.begin(115200); // Set up both ports at this baud. This value is most important for the XBee. Make sure the baud rate matches the config setting of your XBee.
+  Xbee.begin(115200); // Set up both ports at this baud. 
 #endif
   Serial.println(F("ACK CMD ArduCAM Start!")); 
   // set the SPI_CS as an output: 
@@ -177,7 +137,7 @@ void loop() {
     // loop for actually sending the bitstream across the xbee
     while ( (temp != 0xD9) | (temp_last != 0xFF) ) 
     //D9 in hex = 217 in decimal. FF=255 
-    //according to the ArduCam Support, the head of the image data is 0xFF 9xD8
+    //the head of the image data is 0xFF 9xD8
     //and the end of the data is 0xFF 0xD9 
     {
       temp_last = temp; 
@@ -187,9 +147,7 @@ void loop() {
       delayMicroseconds(10); 
     } 
      Serial.println(F("ACK CMD CAM Capture Done.")); 
-     //Clear the capture done flag 
      myCAM.clear_fifo_flag(); 
      start_capture = 0;
-     
   }
  }
